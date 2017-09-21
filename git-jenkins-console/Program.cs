b886b8e85c603648cff010035148ca5742b6abb0
lcruzHS <32069524+lcruzHS@users.noreply.github.com>
2017-09-21 12:20:12 -0400

    Initial commit on master

---
+++ git-jenkins-console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_jenkins_console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args == null) throw new ArgumentNullException("args");
+            if (args.Length == 0) throw new ArgumentOutOfRangeException("args");
+
+            Console.WriteLine(SayHello("Luis"));
+            Console.ReadKey();
+        }
+
+        public static string SayHello(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException("name");
+
+            return string.Format("Hello, {0}", name);
+        }
+    }
+}
---
+++ git-jenkins-consoleTests/ProgramTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using git_jenkins_console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_jenkins_console.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Main_WithNullParameter_ThrowException()
+        {
+            //Arrange             
+
+            //Act
+            Program.Main(null);
+
+            //Assert
+            Assert.Fail("An exception was expected.");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Main_WithEmptyParameter_ThrowException()
+        {
+            //Arrange             
+
+            //Act
+            Program.Main(new string[] { });
+
+            //Assert
+            Assert.Fail("An exception was expected.");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void SayHello_WithNullParameter_ThrowException()
+        {
+            //Arrange             
+
+            //Act
+            var result =  Program.SayHello(null);
+
+            //Assert
+            Assert.Fail("An exception was expected.");
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void SayHello_WithEmptyParameter_ThrowException()
+        {
+            //Arrange             
+
+            //Act
+            var result = Program.SayHello(string.Empty);
+
+            //Assert
+            Assert.Fail("An exception was expected.");
+        }
+    }
+}
